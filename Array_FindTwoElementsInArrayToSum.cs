31decc414babd3d35cec8b27ad47a42e5f7651b3
Rakesh Dangar <rakesh_dangar@hotmail.com>
2025-06-11 13:04:42 -0700

    Update Array_FindTwoElementsInArrayToSum.cs

--- Array_FindTwoElementsInArrayToSum.cs
+++ Array_FindTwoElementsInArrayToSum.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,31 +8,43 @@ namespace ConsoleApplication1
 {
     class Array_FindTwoElementsInArrayToSum
     {
-        public static void Init()
+        public static void Main()
         {
-            List<int> input = new List<int>();
-            foreach (string s in Console.ReadLine().Split(' '))
-            {
-                input.Add(int.Parse(s));
-            }
-
-            input.Sort();
+            int[] input = new int[]{1, 5, 5, 7, 8, 10, 18, 30, 34, 50, 65, 80, 86};
+            
+            Array.Sort(input);
             int sum = 100;
             int start = 0;
-            int end = input.Count - 1;
-            do
-            {
-                if (input[start] + input[end] == sum)
-                {
-                    Console.WriteLine("Value of element at {0} and {1} are adding upto {2}", input[start], input[end], sum);
-                    break;
-                }
-                else if ((input[start] + input[end] < sum))
-                    start++;
-                else
-                    end--;
+            int end = input.Length - 1;
 
-            } while (start <= end);
+            if(input[end-1] + input[end] < sum || input[start] + input[start+1] > sum)
+                Console.WriteLine("There are no two numbers equal to the sum");
+            else
+                do
+                {
+                    Console.WriteLine("Start: " + start.ToString() + " " + "End: " + end.ToString());
+
+                    if(input[start] + input[(end+start)/2] > sum)
+                    {
+                        end = end/2;
+                        Console.WriteLine("First Half");
+                    }
+                    else if(input[(end+start)/2] + input[end] < sum)
+                    {   
+                        start = (end+start)/2;
+                        Console.WriteLine("Second Half");
+                    }
+                    else if (input[start] + input[end] == sum)
+                    {
+                        Console.WriteLine("Value of element at {0} and {1} are adding upto {2}", input[start], input[end], sum);
+                        break;
+                    }
+                    else if ((input[start] + input[end] < sum))
+                        start++;
+                    else
+                        end--;
+
+                } while (start <= end);
         }
     }
 }
