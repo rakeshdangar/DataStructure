4244027514ed2f6f0d664e59a9ba72ede17e2b0b
Rakesh Dangar <rakesh_dangar@hotmail.com>
2025-06-11 11:55:16 -0700

    Update Array_FindMaxDuplicateInArray.cs

--- Array_FindMaxDuplicateInArray.cs
+++ Array_FindMaxDuplicateInArray.cs
@@ -1,4 +1,4 @@
-﻿using System;
+sing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,12 @@ namespace ConsoleApplication1
 {
     class Array_FindMaxDuplicateInArray
     {
-        public static void Init()
+        public static void Main()
         {
-            int[] a = { 1, 2, 3, 3, 4, 5, 3, 6, 3, 3 };
+            int[] a = { 1, 2, 3, 3, 4, 5, 3, 6, 3, 3, 4, 4, 4, 4, 4};
             Array_FindMaxDuplicateInArray f = new Array_FindMaxDuplicateInArray();
             Console.WriteLine(f.FindDuplicate(a).ToString());
+            Console.WriteLine(f.FindIntDuplicate(a).ToString());
         }
 
         public int FindDuplicate(int[] a)
@@ -24,13 +25,35 @@ namespace ConsoleApplication1
                 {
                     ++dict[a[i]];
                     if (a.Length / 2 <= dict[a[i]])
-                        return a[i]; //Count of duplicates is already bigger than rest of the numbers
+                        return a[i];
                 }
                 else
                     dict.Add(a[i], 1);
             }
            
             return dict.OrderByDescending(pair => pair.Value).FirstOrDefault().Key;
+        }
+
+        public int FindIntDuplicate(int[] a)
+        {
+            int[] max = new int[a.Length];
+            for (int i=0; i<a.Length; i++)
+            {
+                max[a[i]]++;
+            }
+
+            int maxDuplicate = 0;
+            int ret = 0;
+            for (int i=0; i<max.Length; i++)
+            {
+                if(max[i] > maxDuplicate)
+                {
+                    maxDuplicate = max[i];
+                    ret = i;
+                }
+            }
+           
+            return ret;
         }
     }
 }
